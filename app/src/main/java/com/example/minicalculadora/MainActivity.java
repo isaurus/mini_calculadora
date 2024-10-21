f51aafb5a2170efe411cd923842bbc6c3c9adca8
Isaac <isaacmartin.dev@gmail.com>
2024-10-21 19:51:27 +0200

    Commit
    - Añadidos los botones por código. Falta la fucionalidad.

--- app/src/main/java/com/example/minicalculadora/MainActivity.java
+++ app/src/main/java/com/example/minicalculadora/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.minicalculadora;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Declaración de TextView
+    TextView txtResultado;
+
+    // Declaración de Button numéricos
+    Button btnUno, btnDos, btnTres, btnCuatro, btnCinco, btnSeis, btnSiete, btnOcho, btnNueve, btnCero, btnPunto;
+
+    // Declaración de Button para operaciones
+    Button btnSumar, btnRestar, btnMultiplicar, btnDividir, btnRoot, btnInverso, btnConmutar, btnIgual, btnBorrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Vinculación de Button a XML
+        btnUno = findViewById(R.id.btnUno);
+        btnDos = findViewById(R.id.btnDos);
+        btnTres = findViewById(R.id.btnTres);
+        btnCuatro = findViewById(R.id.btnCuatro);
+        btnCinco = findViewById(R.id.btnCinco);
+        btnSeis = findViewById(R.id.btnSeis);
+        btnSiete = findViewById(R.id.btnSiete);
+        btnOcho = findViewById(R.id.btnOcho);
+        btnNueve = findViewById(R.id.btnNueve);
+        btnPunto = findViewById(R.id.btnPunto);
+        btnSumar = findViewById(R.id.btnSumar);
+        btnRestar = findViewById(R.id.btnRestar);
+        btnMultiplicar = findViewById(R.id.btnMultiplicar);
+        btnDividir = findViewById(R.id.btnDividir);
+        btnRoot = findViewById(R.id.btnRaizCuadrada);
+        btnInverso = findViewById(R.id.btnInverso);
+        btnConmutar = findViewById(R.id.btnConmutar);
+        btnIgual = findViewById(R.id.btnIgual);
+        btnBorrar = findViewById(R.id.btnBorrar);
+    }
+
+    protected void addNumber(){
+
+    }
+
+    protected void updateResult(){
+
     }
 }
